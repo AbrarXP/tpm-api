56f951d3f7bcb5775c0f0fa23696b4e1f6a184d7
AbrarXP <muhammadabrarkuliah@gmail.com>
2025-06-02 20:47:24 +0700

    env

--- config/database.js
+++ config/database.js
@@ -1,10 +1,12 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
 // Nyambungin db ke BE
-const db = new Sequelize("gizi_db", "admin", "bandengpresto", {
-  host: "34.58.254.65",
-  dialect: "mysql",
-  timezone: '+07:00',
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
+  dialect: "mysql"
 });
 
 export default db;
